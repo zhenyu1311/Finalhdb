30de814bd3e681a2cbd6ea4c95f3f490451f2b70
zhenyu1311 <101508184+zhenyu1311@users.noreply.github.com>
2022-03-20 11:34:14 +0800

    Update settings.py

--- pred/settings.py
+++ pred/settings.py
@@ -127,3 +127,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+$DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
